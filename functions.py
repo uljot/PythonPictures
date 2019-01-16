f8fcf8ec819fd4d950f30526d9eec293067acdec
uljot <joasia.ulman@gmail.com>
2019-01-16 15:31:36 +0100

    removed obsolete parameter in simple_line

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import math
 #original = Image.open("./images/pi.jpg") # load an image from the hard drive
 #blurred = original.filter(ImageFilter.BLUR) # blur the image
 
-def simple_line(intx1, inty1, intx2, inty2, pencil, color, canvas):
+def simple_line(intx1, inty1, intx2, inty2, pencil, color):
     dy = inty2 - inty1
     dx = intx2 - intx1
     m = dy / dx
@@ -18,6 +18,6 @@ def simple_line(intx1, inty1, intx2, inty2, pencil, color, canvas):
 canvas = Image.new('RGBA', (600, 300), color='white')
 pencil = ImageDraw.Draw(canvas)
 
-simple_line(34, 89, 278, 130, pencil, 'green', canvas)
+simple_line(34, 89, 278, 130, pencil, 'green')
 
 canvas.show()
