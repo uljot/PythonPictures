bee7dc3a866ecf02a9e3f8e5b3c05fa86c548179
uljot <joasia.ulman@gmail.com>
2019-01-19 23:49:42 +0100

    removed unnecessary imports and comments

--- functions.py
+++ functions.py
@@ -1,9 +1,6 @@
-from PIL import Image, ImageDraw, ImageColor, ImageFilter  # imports the library
+from PIL import Image, ImageDraw, ImageColor
 from win32api import GetSystemMetrics
 import math, randomcolor, random
-
-#original = Image.open("./images/pi.jpg") # load an image from the hard drive
-#blurred = original.filter(ImageFilter.BLUR) # blur the image
 
 def simple_line(intx1, inty1, intx2, inty2, pencil, color):
     dy = inty2 - inty1
